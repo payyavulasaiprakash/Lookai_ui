677ff64e689295b2c17b988bc3db0b685339e62b
payyavulasaiprakash <payyavulasaiprakash@gmail.com>
2024-08-14 23:36:27 +0530

    ankur_ui_changes_v1

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,51 @@
+<script>
+  import SidebarButton from "./ui/SidebarButton.svelte";
+  import { page } from "$app/stores";
+  import { Icons } from "./../icons";
+
+  let navItems = [
+    {
+      icon: Icons.HOME,
+      tooltip: "Home",
+      route: "/",
+    },
+
+    {
+      icon: Icons.SETTINGS,
+      tooltip: "Settings",
+      route: "/settings",
+    },
+    {
+      icon: Icons.LOGS,
+      tooltip: "Logs",
+      route: "/logs",
+    },
+    {
+      icon: Icons.SearchIcon,
+      tooltip: "Search",
+      route: "/search",
+    },
+    {
+      icon: Icons.FlowChartIcon,
+      tooltip: "convert",
+      route: "/convert",
+    },
+  ];
+</script>
+
+<div
+  class="flex flex-col text-tertiary mx-2 my-4 gap-6 items-center bg-secondary rounded-xl p-6"
+>
+  {#each navItems as { icon, tooltip, route }, i}
+    <SidebarButton
+      {icon}
+      href={route}
+      {tooltip}
+      isSelected={$page.url.pathname == route}
+    />
+  {/each}
+</div>
+;
+
+<style>
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import { Toaster } from "$lib/components/ui/sonner";
+  import { ModeWatcher } from "mode-watcher";
+  import "../app.pcss";
+</script>
+
+<main>
+  <div class="h-dvh w-full flex app-container">
+    <Toaster richColors />
+    <ModeWatcher />
+    <Sidebar />
+    <slot />
+  </div>
+</main>
+
+<style>
+  .app-container {
+    background: linear-gradient(to bottom right, rgba(74, 74, 74), transparent);
+  }
+</style>
